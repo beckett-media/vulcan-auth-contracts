a3194b17138979bc07048d1cd3a369699ec1f545
Eric Nordelo <eric.nordelo39@gmail.com>
2022-02-23 11:38:45 +0100

    feat: add test for upgradeability

--- test/NFTAuthenticator_test.js
+++ test/NFTAuthenticator_test.js
@@ -1,5 +1,7 @@
 const NFTAuthenticator = artifacts.require('NFTAuthenticator');
 
+const { expectRevert, expectEvent, constants } = require('@openzeppelin/test-helpers');
+
 describe('NFTAuthenticator', function () {
   beforeEach(async () => {
     await deployments.fixture(['nft_authenticator']);
@@ -10,5 +12,24 @@ describe('NFTAuthenticator', function () {
 
   it('should be deployed', async () => {
     assert.isOk(this.authenticator.address);
+  });
+
+  describe('upgradeability', () => {
+    it("can't upgrade with wrong accounts", async () => {
+      const { bob } = await getNamedAccounts();
+      const revertMessage = 'Ownable: caller is not the owner';
+
+      await expectRevert(this.authenticator.upgradeTo(constants.ZERO_ADDRESS, { from: bob }), revertMessage);
+    });
+
+    it('can upgrade with right account', async () => {
+      const { deployer } = await getNamedAccounts();
+
+      let newImplementation = await NFTAuthenticator.new();
+
+      let tx = await this.authenticator.upgradeTo(newImplementation.address, { from: deployer });
+
+      expectEvent(tx, 'Upgraded', { implementation: newImplementation.address });
+    });
   });
 });
